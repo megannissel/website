39eba43677becb38aa2917e98e01a7fa1a7c7705
Megan Nissel <megnissel@gmail.com>
2023-12-10 20:02:23 -0500

    homepage base

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+from datetime import datetime, timedelta
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home():
+  years = (datetime.utcnow().date() - datetime(2017,1,1).date()).days // 365
+  return render_template('home.html',
+    years=years,
+  )
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/resume')
+def resume():
+  return render_template('resume.html')
