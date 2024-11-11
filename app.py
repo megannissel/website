73df12ea4dfbfed106002c89599ab15768114f8a
Megan Nissel <megnissel@gmail.com>
2024-11-11 15:18:17 -0500

    reorganize portfolio templates into subdir

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
+import os
 from datetime import datetime, timedelta
 
 from flask import Flask
+from flask_login import LoginManager
 
 from lib.globals import register_jinja_globals
+from services.auth import register_auth_endpoints, load_user
 from services.index import register_index_endpoints
+from services.recipes import register_recipe_endpoints
+from services.sql import Session
 
 
 def initialize_app():
   app = Flask(__name__)
 
+  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
   register_jinja_globals(app)
+  register_auth_endpoints(app)
   register_index_endpoints(app)
+  register_recipe_endpoints(app)
+
+  login_manager = LoginManager()
+  login_manager.login_view = "login"
+  login_manager.user_loader(load_user)
+  login_manager.init_app(app)
 
   return app
 
 app = initialize_app()
+
+@app.teardown_appcontext
+def cleanup(resp_or_exc):
+  Session.remove()
