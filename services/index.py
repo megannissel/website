2be49018a1a84df8b1b19ff56cec056ed43d4ae5
Megan Nissel <megnissel@gmail.com>
2024-11-11 15:18:17 -0500

    reorganize portfolio templates into subdir

--- services/index.py
+++ services/index.py
@@ -8,14 +8,14 @@ def register_index_endpoints(app):
   @app.route('/')
   def home():
     years = (datetime.utcnow().date() - datetime(2017,1,1).date()).days // 365
-    return render_template('home.html',
+    return render_template('portfolio/home.html',
       years=years,
     )
 
   @app.route('/about')
   def about():
-    return render_template('about.html')
+    return render_template('portfolio/about.html')
 
   @app.route('/resume')
   def resume():
-    return render_template('resume.html')
+    return render_template('portfolio/resume.html')
